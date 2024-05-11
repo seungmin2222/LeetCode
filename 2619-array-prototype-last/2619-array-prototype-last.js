eb487c1f5701e4318ed20db52b74acb8c2faa501
seungmin2222 <95403275+seungmin2222@users.noreply.github.com>
2024-05-11 13:56:40 +0900

    Time: 43 ms (90.37%), Space: 48.3 MB (83.22%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -2,7 +2,7 @@
  * @return {null|boolean|number|string|Array|Object}
  */
 Array.prototype.last = function () {
-   return this.length === 0 ? -1 : this.pop()
+   return this.length === 0 ? -1 : this.pop();
 };
 
 /**
