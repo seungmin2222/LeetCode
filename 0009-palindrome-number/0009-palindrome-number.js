3dd34bf37afebb3c586b78a3c7b4eead4011d82e
seungmin2222 <95403275+seungmin2222@users.noreply.github.com>
2024-05-13 08:57:23 +0900

    Time: 134 ms (56.81%), Space: 57 MB (67.74%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -5,11 +5,10 @@
 var isPalindrome = function(x) {
   if (x.toString().length === 1) {
     return true;
-  } else {
-    if (x < 0) {
+  } else if (x < 0) {
       return false;
-    }
   }
+  
   x = x.toString();
    for (let i = 0; i < x.length/2; i++) {
       if(x[i] !== x[x.length-1-i]){
