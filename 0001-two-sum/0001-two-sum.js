4a146fc31e6931fc26d3268d3ce17731b0f589a1
seungmin2222 <95403275+seungmin2222@users.noreply.github.com>
2024-05-12 10:50:42 +0900

    Time: 65 ms (55.79%), Space: 50.5 MB (50.95%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,15 +1,11 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
 const twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
+    const obj = {};
+    
+    for (let i = 0; i < nums.length; i++) {
+        const diff = target - nums[i];
+        if (diff in obj) {
+           return [obj[diff], i];
         }
+        obj[nums[i]] = i;
     }
 };
-
