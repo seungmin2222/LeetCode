c4b7964024444b5a25b2d13300d402490957dc11
seungmin2222 <95403275+seungmin2222@users.noreply.github.com>
2024-05-14 08:11:54 +0900

    Time: 154 ms (5.04%), Space: 49.6 MB (79.93%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -14,9 +14,10 @@ var isValid = function(s) {
     const small = clonedValue.lastIndexOf(arr[0]);
     const middle = clonedValue.lastIndexOf(arr[2]);
     const big = clonedValue.lastIndexOf(arr[4]);
-    const pair = arr[arr.indexOf(clonedValue[Math.max(small,middle,big)])+1];
-    if (clonedValue[Math.max(small,middle,big)+1] === pair) {
-        clonedValue.splice(Math.max(small,middle,big),2);
+    const lastIndexNum = Math.max(small,middle,big);
+    const pair = arr[arr.indexOf(clonedValue[lastIndexNum])+1];
+    if (clonedValue[lastIndexNum+1] === pair) {
+        clonedValue.splice(lastIndexNum,2);
     } else {
       return false;
     }
