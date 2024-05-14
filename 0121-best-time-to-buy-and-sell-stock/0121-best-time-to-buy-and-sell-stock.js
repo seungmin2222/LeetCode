cfc0bafd61c6dff0cec06f31a1c884f1096cc381
seungmin2222 <95403275+seungmin2222@users.noreply.github.com>
2024-05-14 09:46:58 +0900

    Time: 76 ms (48.30%), Space: 59.3 MB (38.83%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  let minPrice = Infinity;
+  let minPrice = Math.max(...prices);
   let profitNum = 0;
   
   for (let i = 0; i < prices.length; i ++) {
