9aa00911ea9bcdf96b156093b9d70c0591acd0b4
seungmin2222 <95403275+seungmin2222@users.noreply.github.com>
2024-06-07 10:25:31 +0900

    Time: 53 ms (71.76%), Space: 48.1 MB (99.07%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -10,6 +10,7 @@ var longestCommonPrefix = function(strs) {
   for (let i = 1; i < strs.length; i++) {
     while (strs[i].indexOf(prefix) !== 0) {
       prefix = prefix.substring(0, prefix.length - 1);
+
       if (prefix === "") return "";
     }
   }
