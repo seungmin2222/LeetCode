c3a3994a13f10cec12eaff269cbb2be0f8c69738
seungmin2222 <95403275+seungmin2222@users.noreply.github.com>
2024-06-08 22:35:35 +0900

    Time: 62 ms (68.74%), Space: 51.9 MB (78.15%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    let count = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[count] = nums[i];
+            count++;
+        }
+    }
+
+    return count;
+};
