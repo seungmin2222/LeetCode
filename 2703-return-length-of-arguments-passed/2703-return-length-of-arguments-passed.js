5d2752ef57744d6879fe199e78dccf97a8b34767
seungmin2222 <95403275+seungmin2222@users.noreply.github.com>
2024-06-09 18:30:43 +0900

    Time: 61 ms (8.42%), Space: 48.8 MB (41.56%) - LeetHub

--- 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -1,11 +1,3 @@
-/**
- * @param {...(null|boolean|number|string|Array|Object)} args
- * @return {number}
- */
-var argumentsLength = function(...args) {
+let argumentsLength = function(...args) {
     return args.length;
 };
-
-/**
- * argumentsLength(1, 2, 3); // 3
- */
