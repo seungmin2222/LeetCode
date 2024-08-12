ca21e4802153c13778793df2390067f7dc1263dd
seungmin2222 <95403275+seungmin2222@users.noreply.github.com>
2024-08-12 14:41:16 +0900

    Time: 39 ms (99.44%), Space: 52.1 MB (59.33%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -8,7 +8,7 @@ var search = function(nums, target) {
   let right = nums.length - 1;
   
   while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
+    const mid = Math.floor((left + right) / 2);
     
     if (nums[mid] === target) {
       return mid;
