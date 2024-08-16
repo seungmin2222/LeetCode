cfd8f3eba78f9a2c3135be3f67dd9d85ee20e612
seungmin2222 <95403275+seungmin2222@users.noreply.github.com>
2024-08-16 15:16:52 +0900

    Time: 41 ms (98.86%), Space: 49.1 MB (75.77%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var backspaceCompare = function(s, t) {
+  const a = [...s];
+  const b = [...t];
+  
+  const c = [];
+  const d = [];
+  
+  for (let i = 0; i < s.length; i++) {
+    if (a[i] === '#') {
+      c.pop();
+    } else {
+      c.push(a[i]);
+    }
+  }
+  
+  for (let i = 0; i < t.length; i++) {
+    if (b[i] === '#') {
+      d.pop();
+    } else {
+      d.push(b[i]);
+    }
+  }
+  
+  return c.toString() === d.toString();
+  
+};
