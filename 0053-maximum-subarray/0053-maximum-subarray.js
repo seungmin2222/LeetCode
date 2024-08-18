694e58420a21b9d263ac19936c199cf0906e020d
SeungMin Lee <wooju1701@hanmail.net>
2024-08-19 00:23:43 +0900

    Time: 73 ms (52.42%), Space: 57.8 MB (45.00%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  let currentNum = nums[0];
+  let maxNum = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    currentNum = Math.max(nums[i], currentNum + nums[i]);
+    maxNum = Math.max(maxNum, currentNum);
+  }
+
+  return maxNum;
+};
