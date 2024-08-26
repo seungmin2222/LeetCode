c1bc357906f7257dfeddce6f99a0ba5b65a86eb0
SeungMin Lee <wooju1701@hanmail.net>
2024-08-26 14:12:44 +0900

    Time: 2455 ms (5.08%), Space: 57.7 MB (94.13%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[][]}
+ */
+var updateMatrix = function(mat) {
+  let newMat = [...mat];
+  const a = [];
+  const b = [];
+  
+  for (let i = 0; i < mat.length; i++) {
+    for (let j = 0; j < mat[i].length; j++) {
+      if (mat[i][j] === 0) {
+        a.push([i,j]);
+        newMat[i][j] = 0;
+      } else {
+        b.push([i,j]);
+        newMat[i][j] = Infinity;
+      }
+    }
+  }
+  
+  for (let i = 0; i < b.length; i++) {
+    let minNum = Infinity;
+    for (let j = 0; j < a.length; j++) {
+      const c = Math.abs(b[i][0] - a[j][0]);
+      const d = Math.abs(b[i][1] - a[j][1]);
+      if (minNum > c + d) {
+        minNum = c + d;
+      }
+    }
+
+    newMat[b[i][0]][b[i][1]] = minNum;
+  }
+  
+  return newMat;
+};
