067156cc8f8102879690a519348c5180dd1a8c70
SeungMin Lee <wooju1701@hanmail.net>
2024-08-29 23:18:09 +0900

    Time: 60 ms (56.78%), Space: 51.9 MB (42.83%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root, num) {
+  let maxNum = 0;
+
+  function depth(node, num) {
+    if (!node) return 0;
+    num++
+    
+    let leftDepth = depth(node.left, num);
+    let rightDepth = depth(node.right, num);
+    
+    if (maxNum < num) {
+        maxNum = num;
+    }
+  }
+  depth(root, 0);
+  
+  return maxNum;
+};
