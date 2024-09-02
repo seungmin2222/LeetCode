3e628474be7038038103d7a820a905263474a2b4
SeungMin Lee <wooju1701@hanmail.net>
2024-09-02 10:59:29 +0900

    Time: 56 ms (40.80%), Space: 49 MB (50.10%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,5 +3,27 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    return nums.sort();
+  let zero = '';
+  let one = '';
+  let two = '';
+  
+  for (let i = 0; i < nums.length; i ++) {
+    if (nums[i] === 0) {
+      zero++;
+    } else if (nums[i] === 1) {
+      one++;
+    } else if (nums[i] === 2) {
+      two++;
+    }
+  }
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (i < zero) {
+      nums[i] = 0;
+    } else if (i < zero + one) {
+      nums[i] = 1;
+    } else {
+      nums[i] = 2;
+    }
+  }
 };
