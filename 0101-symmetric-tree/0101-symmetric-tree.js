aff150c3d6b1a63d2ad94c0f7651b8f4b9be0dad
SeungMin Lee <wooju1701@hanmail.net>
2024-09-12 01:04:39 +0900

    Time: 61 ms (54.45%), Space: 52 MB (16.62%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+  function isMirror(tree1, tree2) {
+      if (!tree1 && !tree2) {
+          return true;
+      }
+      if (!tree1 || !tree2) {
+          return false;
+      }
+      return (tree1.val === tree2.val)
+          && isMirror(tree1.right, tree2.left)
+          && isMirror(tree1.left, tree2.right);
+  }
+
+  if (!root) {
+      return true;
+  }
+
+  return isMirror(root.left, root.right);
+};
