a7fde82b43c4290933eca199a18412be0a34a429
SeungMin Lee <wooju1701@hanmail.net>
2024-09-15 02:13:19 +0900

    Time: 76 ms (28.40%), Space: 56.7 MB (22.49%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+    if (!subRoot) return true;
+    if (!root) return false;
+    
+    if (isSameTree(root, subRoot)) return true;
+    
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+};
+
+var isSameTree = function(p, q) {
+    if (!p && !q) return true;
+    if (!p || !q || p.val !== q.val) return false;
+    
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
