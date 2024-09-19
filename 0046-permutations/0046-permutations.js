be4f0e7f8520f46d17fb19240dfe4937dbeaf181
SeungMin Lee <wooju1701@hanmail.net>
2024-09-20 02:22:17 +0900

    Time: 68 ms (63.04%), Space: 53.8 MB (33.04%) - LeetHub

---
+++ 0046-permutations/0046-permutations.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+  const result = [];
+
+  const dfs = (i, arr) => {
+    if (i === arr.length) {
+      return result.push([...arr]);
+    }
+    
+    for (let j = i; j < arr.length; j++) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      dfs(i + 1, arr);
+      [arr[j], arr[i]] = [arr[i], arr[j]];
+    }
+  };
+  
+  dfs(0, nums);
+
+  return result;
+};
