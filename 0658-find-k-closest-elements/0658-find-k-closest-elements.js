36b098161a5bc267205f84500703f60db55a7f2e
SeungMin Lee <wooju1701@hanmail.net>
2024-10-23 20:54:56 +0900

    Time: 0 ms (100.00%), Space: 56 MB (60.40%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function(arr, k, x) {
+    if (arr.length <= k) return arr;
+    
+    let left = 0;
+    let right = arr.length - k;
+    
+    while (left < right) {
+        const mid = Math.floor(left + (right - left) / 2);
+        
+        if (x - arr[mid] > arr[mid + k] - x) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    
+    return arr.slice(left, left + k);
+};
