a937c71183dd3b74c778f2c1bf53cd747c7b1909
SeungMin Lee <wooju1701@hanmail.net>
2024-10-29 16:59:29 +0900

    Time: 6 ms (14.24%), Space: 51.3 MB (19.78%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    const uniqueArr1 = [];
+    const uniqueArr2 = [];
+    const unique = [];
+  
+    nums1.forEach((element) => {
+        if (!uniqueArr1.includes(element)) {
+            uniqueArr1.push(element);
+        }
+    });
+  
+    nums2.forEach((element) => {
+        if (!uniqueArr2.includes(element)) {
+            uniqueArr2.push(element);
+        }
+    });
+  
+    
+    uniqueArr2.forEach((element) => {
+        if (uniqueArr1.includes(element)) {
+            unique.push(element);
+        }
+    });
+  
+  return unique;
+  
+};
