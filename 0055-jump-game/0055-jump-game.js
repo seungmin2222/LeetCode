a95d0c41d75bfeb6b18d5fbddcc69b9472e6c402
SeungMin Lee <wooju1701@hanmail.net>
2024-11-03 15:27:57 +0900

    Time: 3 ms (40.89%), Space: 53.7 MB (32.93%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+  let maxNum = 0;
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (i > maxNum) return false; 
+    
+    maxNum = Math.max(maxNum, i + nums[i]);
+    
+    if (maxNum >= nums.length - 1) return true;
+  }
+  
+  return false;
+};
