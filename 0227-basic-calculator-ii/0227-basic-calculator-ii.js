0c1ff346e0cfd2fd148268a6ec27ae3e84e2f040
SeungMin Lee <wooju1701@hanmail.net>
2024-11-05 03:20:57 +0900

    Time: 34 ms (46.82%), Space: 59.1 MB (49.74%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(s) {
+  const stack = [];
+  let num = 0;
+  let sign = '+';
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+
+    if (!isNaN(parseInt(char)) && char !== ' ') {
+      num = num * 10 + parseInt(char);
+    }
+
+    if (['+', '-', '*', '/'].includes(char) || i === s.length - 1) {
+      switch (sign) {
+        case '+':
+          stack.push(num);
+          break;
+        case '-':
+          stack.push(-num);
+          break;
+        case '*':
+          stack.push(stack.pop() * num);
+          break;
+        case '/':
+          stack.push(Math.trunc(stack.pop() / num));
+          break;
+      }
+      sign = char;
+      num = 0;
+    }
+  }
+
+  return stack.reduce((a, b) => a + b, 0);
+};
