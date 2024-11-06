caca834ec1d669c123a0b5fb3404f256e7667b30
SeungMin Lee <wooju1701@hanmail.net>
2024-11-07 00:05:19 +0900

    Time: 55 ms (30.00%), Space: 48.3 MB (90.00%) - LeetHub

--- 0984-string-without-aaa-or-bbb/0984-string-without-aaa-or-bbb.js
+++ 0984-string-without-aaa-or-bbb/0984-string-without-aaa-or-bbb.js
@@ -4,39 +4,39 @@
  * @return {string}
  */
 var strWithout3a3b = function(a, b) {
-  let word = '';
+  let words = '';
   
   while (a > 0 || b > 0) {
     if (a > b) {
       if (a > 1) {
-        word += 'aa';
+        words += 'aa';
         a -= 2;
       } else {
-        word += 'a';
+        words += 'a';
         a -= 1;
       }
       if (b > 0) {
-        word += 'b';
+        words += 'b';
         b -= 1;
       }
     } else if (b > a) {
       if (b > 1) {
-        word += 'bb';
+        words += 'bb';
         b -= 2;
       } else {
-        word += 'b';
+        words += 'b';
         b -= 1;
       }
       if (a > 0) {
-        word += 'a';
+        words += 'a';
         a -= 1;
       }
     } else {
-      word += 'ab';
+      words += 'ab';
       a -= 1;
       b -= 1;
     }
   }
   
-  return word;
+  return words;
 };
