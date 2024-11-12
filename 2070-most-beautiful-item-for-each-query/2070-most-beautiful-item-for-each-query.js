ce44e22d56f56c52da773f74f275b0209c4b780b
SeungMin Lee <wooju1701@hanmail.net>
2024-11-13 03:02:04 +0900

    Time: 77 ms (50.00%), Space: 85.2 MB (28.57%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.js
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.js
@@ -7,22 +7,30 @@ var maximumBeauty = function(items, queries) {
   items.sort((a, b) => a[0] - b[0]);
 
   let maxBeauty = 0;
+  
   for (let i = 0; i < items.length; i++) {
     maxBeauty = Math.max(maxBeauty, items[i][1]);
     items[i][1] = maxBeauty;
   }
 
-  const queryWithIndex = queries.map((value, index) => [value, index]);
-  queryWithIndex.sort((a, b) => a[0] - b[0]);
+  const result = new Array(queries.length);
 
-  const result = new Array(queries.length).fill(0);
-  let j = 0;
+  for (let i = 0; i < queries.length; i++) {
+    let left = 0, right = items.length - 1;
+    let maxNum = 0;
 
-  for (let [query, originalIndex] of queryWithIndex) {
-    while (j < items.length && items[j][0] <= query) {
-      j++;
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);
+
+      if (items[mid][0] <= queries[i]) {
+        maxNum = items[mid][1]; 
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
     }
-    if (j > 0) result[originalIndex] = items[j - 1][1];
+    
+    result[i] = maxNum;
   }
 
   return result;
