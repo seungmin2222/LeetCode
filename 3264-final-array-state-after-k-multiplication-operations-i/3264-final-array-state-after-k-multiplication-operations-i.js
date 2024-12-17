11600c47a65044db6fd6c6bd6662415e49cfe967
SeungMin Lee <wooju1701@hanmail.net>
2024-12-18 00:00:28 +0900

    Time: 0 ms (100.00%), Space: 52.6 MB (30.19%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.js
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.js
@@ -7,7 +7,7 @@
 var getFinalState = function(nums, k, multiplier) {
   for (let i = 0; i < k; i++) {
     let minIndex = 0;
-    
+
     for (let j = 1; j < nums.length; j++) {
       if (nums[j] < nums[minIndex]) {
         minIndex = j;
