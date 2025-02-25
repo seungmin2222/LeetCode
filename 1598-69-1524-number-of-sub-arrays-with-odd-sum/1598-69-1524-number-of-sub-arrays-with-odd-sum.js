f118fb666c81d9a61311acbcdc681304701943bc
SeungMin Lee <wooju1701@hanmail.net>
2025-02-25 16:33:05 +0900

    Time: 4 ms, Memory: 65.3 MB - LeetHub

---
+++ 1598-69-1524-number-of-sub-arrays-with-odd-sum/1598-69-1524-number-of-sub-arrays-with-odd-sum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var numOfSubarrays = function(arr) {
+    let count = 0;
+    let prefixSum = 0;
+    
+    let even = 1;
+    let odd = 0;
+    
+    const MOD = 1000000007; // 10^9 + 7
+    
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+        prefixSum += arr[i];
+        
+        if (prefixSum & 1) {
+            count = (count + even) % MOD;
+            odd = (odd + 1) % MOD;
+        } else {
+            count = (count + odd) % MOD;
+            even = (even + 1) % MOD;
+        }
+    }
+    
+    return count;
+};
