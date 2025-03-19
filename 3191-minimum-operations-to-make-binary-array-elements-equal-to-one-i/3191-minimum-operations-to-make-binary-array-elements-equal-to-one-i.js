a441196a9942622f0dee1506402ea87d05d7d971
SeungMin Lee <wooju1701@hanmail.net>
2025-03-19 16:05:33 +0900

    Time: 7 ms (54.55%), Space: 64.9 MB (30.91%) - LeetHub

--- 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.js
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.js
@@ -2,28 +2,27 @@
  * @param {number[]} nums
  * @return {number}
  */
-var minOperations = function (nums) {
+var minOperations = function(nums) {
     const n = nums.length;
-
+    
     if (n < 3) {
         return nums.every(num => num === 1) ? 0 : -1;
     }
-
+    
     let operations = 0;
-    let copy = [...nums];
-
+    
     for (let i = 0; i < n - 2; i++) {
-        if (copy[i] === 0) {
-            copy[i] = 1;
-            copy[i + 1] ^= 1;
-            copy[i + 2] ^= 1;
+        if (nums[i] === 0) {
+            nums[i] ^= 1;
+            nums[i+1] ^= 1;
+            nums[i+2] ^= 1;
             operations++;
         }
     }
-
-    if (copy[n - 2] === 1 && copy[n - 1] === 1) {
+    
+    if (nums[n-2] === 1 && nums[n-1] === 1) {
         return operations;
     }
-
+    
     return -1;
 };
