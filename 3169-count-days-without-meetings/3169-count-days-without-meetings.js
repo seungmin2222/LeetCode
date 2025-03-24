5e395cb1f0bb12b626a807f14002a0ab9507ac02
SeungMin Lee <wooju1701@hanmail.net>
2025-03-24 15:46:38 +0900

    Time: 71 ms (80.65%), Space: 82.2 MB (51.61%) - LeetHub

---
+++ 3169-count-days-without-meetings/3169-count-days-without-meetings.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} days
+ * @param {number[][]} meetings
+ * @return {number}
+ */
+var countDays = function (days, meetings) {
+    meetings.sort((a, b) => a[0] - b[0]);
+
+    let mergedMeetings = [];
+
+    for (let meeting of meetings) {
+        if (mergedMeetings.length === 0 || mergedMeetings[mergedMeetings.length - 1][1] < meeting[0]) {
+            mergedMeetings.push(meeting);
+        } else {
+            mergedMeetings[mergedMeetings.length - 1][1] = Math.max(mergedMeetings[mergedMeetings.length - 1][1], meeting[1]);
+        }
+    }
+
+    let bookedDays = 0;
+    for (let meeting of mergedMeetings) {
+        bookedDays += meeting[1] - meeting[0] + 1;
+    }
+
+    return days - bookedDays;
+};
