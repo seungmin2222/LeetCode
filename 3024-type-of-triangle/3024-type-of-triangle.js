6ee4c8a7aad0b5cba903bfdadbea20583086d96f
SeungMin Lee <wooju1701@hanmail.net>
2025-08-06 20:27:13 +0900

    Time: 0 ms (100%), Space: 54.8 MB (61.3%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var triangleType = function(nums) {
+    const [a, b, c] = nums;
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        return "none";
+    }
+
+    if (a === b && b === c) return "equilateral";
+    if (a === b || b === c || a === c) return "isosceles";
+    return "scalene";
+};
