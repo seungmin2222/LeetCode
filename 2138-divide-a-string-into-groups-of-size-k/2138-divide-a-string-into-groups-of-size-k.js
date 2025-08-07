4713a5b7ffd3f72417feeae400640e8f7910e223
SeungMin Lee <wooju1701@hanmail.net>
2025-08-07 17:42:52 +0900

    Time: 1 ms (40.87%), Space: 53.4 MB (83.97%) - LeetHub

---
+++ 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @param {character} fill
+ * @return {string[]}
+ */
+var divideString = function(s, k, fill) {
+    const result = [];
+
+    for (let i = 0; i < s.length; i += k) {
+        let group = s.slice(i, i + k);
+
+        if (group.length < k) {
+            group = group + fill.repeat(k - group.length);
+        }
+
+        result.push(group);
+    }
+    
+    return result;
+};
